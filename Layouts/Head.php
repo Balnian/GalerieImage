e08dec57ef3368d88dfebb3d9a7b1cbd323711c9
unknown <201250541@P104-14.clg.priv>
2015-11-23 17:20:15 -0500

    Init

---
+++ Layouts/Head.php
@@ -0,0 +1,20 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+	
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script>
+</head>
+<body>
+	<?php 
+	include_once "Menu.php";
+
+
+
